17d7d4d23ae3811cbf25e558b3950cedf065159c
Roger Chappel <roger@users.noreply.github.com>
2026-05-02 14:43:48 +1000

    feat(api): export programmatic core API

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export { compatibilityReport, supportedTargets } from './core/compatibility.js';
+export { installSkill } from './core/install.js';
+export { buildManifest } from './core/manifest.js';
+export { parseSkillMarkdown, readSkillMetadata } from './core/metadata.js';
+export { packSkill, readPack } from './core/pack.js';
+export { validateSkill } from './core/validation.js';
