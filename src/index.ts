a46647c0051bdfd441e3f1d6fc2bf45c9b723d8e
Roger Chappel <miscanalysis@gmail.com>
2026-05-10 06:36:40 +1000

    feat: expose public library API

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { parseFrontmatter } from "./frontmatter.js";
+export { installSkillPack } from "./install.js";
+export { createSkillFromDocs } from "./pack.js";
+export { createCompatibilityReport } from "./report.js";
+export { defaultTargetDir, parseTarget, targetDescriptions } from "./targets.js";
+export { validateSkillPack } from "./validator.js";
+export type { AgentTarget, CompatibilityReport, Diagnostic, InstallResult, Skill, SkillMetadata, ValidationResult } from "./types.js";
